ec2fe369dc1f27bd488166dc960439a2b53ed8b9
Vatsalya Tandon <vatsalyatandon@gmail.com>
2025-11-29 10:00:47 +0530

    Initial Commit

---
+++ Kleen/KleenApp.swift
@@ -0,0 +1,17 @@
+//
+//  KleenApp.swift
+//  Kleen
+//
+//  Created by Vatsalya Tandon on 29/11/25.
+//
+
+import SwiftUI
+
+@main
+struct KleenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KleenTests/KleenTests.swift
@@ -0,0 +1,17 @@
+//
+//  KleenTests.swift
+//  KleenTests
+//
+//  Created by Vatsalya Tandon on 29/11/25.
+//
+
+import Testing
+@testable import Kleen
+
+struct KleenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
